6cabf4fb04f9678c7cadf8511c7646de6be65065
gzamfir-ca <gzamfir_ca@icloud.com>
2026-04-29 13:21:07 -0400

    initial commit

---
+++ spec/image/organizer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Image::Organizer do
+  it "has a version number" do
+    expect(Image::Organizer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(true).to eq(true)
+  end
+end
